d7ae598e1fd8a7e456c1b03fdbdc8b97ca613cec
jiangjunjie666 <2631261619@qq.com>
2023-07-19 10:28:49 +0800

    没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>根组件</h1>
+  </div>
+</template>
+
+<script>
+let a = '111'
+</script>
+
+<style scoped lang="scss">
+div {
+  width: 100px;
+  height: 100px;
+  background-color: #333;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+// @ts-expect-error
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const fun = () => {
+
+  console.log(111);
+}
+fun()
